ccd6f1e35d127aba67cd6f647bd1894f73add0af
Shay Bushary <shaybush93@gmail.com>
2024-01-31 18:16:15 +0200

    chapter 15

---
+++ app/ui/copyButton.tsx
@@ -0,0 +1,43 @@
+import clsx from 'clsx';
+import { useState } from 'react';
+import myStyles from './copyButton.module.css';
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  path: string;
+}
+
+export function CopyButton({ path, className }: ButtonProps) {
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  const handleClick = () => {
+    navigator.clipboard.writeText(path)
+    setIsAnimating(true);
+    setTimeout(() => {
+        setIsAnimating(false);
+    }, 1000); // Switch back after 1 second
+};
+
+  return (
+    <button
+    type='button'
+    onClick={() => handleClick()}
+      className={clsx(
+        'flex h-10 items-center rounded-lg px-2 text-sm font-medium text-black transition-colors focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 aria-disabled:cursor-not-allowed aria-disabled:opacity-50',
+        className,
+      )}
+    >
+      {isAnimating ? (
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="green" className={myStyles.iconAnimation} viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0"/>
+        <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1z"/>
+        <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0z"/>
+      </svg>
+      ) : (
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-clipboard" viewBox="0 0 16 16">
+        <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1z"/>
+        <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0z"/>
+      </svg>
+      )}
+    </button>
+  );
+}
